54af769ad01b9f547d77baeb345bf7e0c2a30530
huynguyen.uit@gmail.com <huynguyen.uit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-09-04 16:25:00 +0000

    Demo Gunny

---
+++ trunk/game/Demo Gunny code tay/Soul-Leader/Soul-Leader/bin/Debug/AppX/js/Player.js
@@ -0,0 +1,42 @@
+﻿var RADIUS = 10;
+
+function Player() {
+    this.x = 300
+    this.y = 300;
+    this.sprite = new Image();
+    this.sprite.src = "images/player.png";
+    this.Height = this.sprite.height;
+    this.Width = this.sprite.width;
+    this.angle = 0;
+    this.ball = null;
+}
+
+Player.prototype.draw = function(context)
+{
+    context.drawImage(this.sprite, this.x, this.y);
+    if (this.ball != null)
+        this.ball.draw(context);
+}
+
+Player.prototype.update = function () {
+    if (this.ball != null)
+        this.ball.update();
+}
+
+Player.prototype.setBall = function (_ball) {
+    this.ball = _ball;
+}
+
+
+Player.prototype.fire = function (targetX, targetY) {
+    var dx = targetX - this.x;
+    var dy = targetY - this.y;
+    this.angle = Math.atan2(dy, dx);
+    var distance = Math.sqrt(dx*dx + dy*dy);
+    var dirX = Math.cos(this.angle);
+    var dirY = Math.sin(this.angle);
+
+    var startX = this.x + dirX;
+    var startY = this.y + dirY;
+    this.ball.Set(startX, startY, dirX, dirY,distance);
+}
---
+++ trunk/game/Demo Gunny code tay/Soul-Leader/Soul-Leader/js/map.js
@@ -0,0 +1,48 @@
+﻿function map() {
+    this.x = 0
+    this.y = 0;
+    this.width = 800;
+    this.height = 600;
+
+    this.img = new Image();
+    this.img.src = "images/map.png";
+
+    
+    var buffer = document.createElement("canvas");
+    buffer.width = this.width;
+    buffer.height = this.height;
+    var context = buffer.getContext("2d");
+    context.drawImage(this.img, 0, 0, this.width, this.height);
+
+    imageData = context.getImageData(0, 0, buffer.width, buffer.height);
+    this.draw = function (context) {
+        context.drawImage(buffer, 300, 0, this.width, this.height);
+    }
+
+    this.contain = function (x, y) {
+        if (!imageData)
+            return false;
+        var index = Math.floor((x + y * this.width) * 4 + 3);
+        return imageData.data[index] && imageData.data[index] != 0;
+    }
+
+    this.collide = function (ball) {
+        if (ball == null)
+            return false;
+        var x = Math.floor(ball.x - 300);
+        var y = Math.floor(ball.y);
+        if (this.contain(x, y)) {
+            context.save();
+            context.globalCompositeOperation = "destination-out";
+            context.fillStyle = "rgba(0,0,0,1)";
+            context.beginPath();
+            context.arc(x, y, 50, 0, Math.PI * 2, true);
+            context.fill();
+            context.restore();
+            imageData = context.getImageData(0, 0, this.width, this.height);
+            return true;
+        }
+        return false;
+    };
+
+}
