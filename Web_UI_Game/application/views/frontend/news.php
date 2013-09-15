6da3ad7a0defa066ce2f897b5813030f14d471b8
mr.hiepuit <Mr.hiepuit@gmail.com>
2013-09-15 01:49:48 +0000

    Tạo file news.php trong views/frontend/news.php để hiển thị tin tức cho người dùng thông tin mới nhất.

---
+++ Web_UI_Game/application/views/frontend/news.php
@@ -0,0 +1,22 @@
+<?php
+    foreach($news as $new){
+        echo ' <div class="post" >
+                <div class = "post-img">
+                    <a href="'.$frontend.$new['news_image'].'"
+                    alt = "'.$new['news_image'].'" width = "200" height = "130" ></a>
+                </div> 
+                <div class="excerpt">
+                    <div> 
+                        <h3><a href="'.$frontend.'news'.$new['news_id'].'">'.$new['news_name'].'</a></h3>                    
+                    </div>
+                    <div class="post-info">'.$new['news_post'].'</div>
+                    <div class="article-content">'.word_limiter(preg_replace('|<img(.*)src="(.*)"(.*)>|is','',$new['news_content']),100).'
+                        <a href="'.$frontend.'news_detail/'.$new['news_id'].'"
+                        class="read">read</a>
+                    </div>
+                </div>
+        </div>';
+    }?>
+    <div class="clear"></div>
+ <?php echo $this->pagination->create_links();
+ ?>
