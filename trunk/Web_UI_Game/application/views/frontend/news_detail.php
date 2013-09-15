b592e32797a6ddf5d1a7e884f8e9d2fbcb939b67
Mr.hiepuit@gmail.com <Mr.hiepuit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-09-15 01:50:56 +0000

    Tạo file news_detail.php để hiển thị chi tiết từng thông tin cụ thể khi người dùng click vào một thông tin bất kì trên web

---
+++ trunk/Web_UI_Game/application/views/frontend/news_detail.php
@@ -0,0 +1,58 @@
+<div id="news-single">
+    <div id="news-wrapper">
+        
+        
+        <div class="news-post">
+            <h2 class="news-posttitle"><?php 
+                                            echo $article['news_name']; 
+                                        ?>
+            </h2>
+            
+            <!-- start: content -->
+            <div class="new_content">
+             <!-- noi dung tom tat --!>
+                  <h4 style="text-align: justify;">
+                      <span style="font-size: 1.5em;">
+                        <?php word_limiter($article['news_content'], 20); ?>
+                      </span>                
+                  </h4>
+            <!-- noi dung tom tat --!>
+            <!-- noi dung --!>
+                <div style="text-align: justify;">
+                    <p align="justify">
+                        <?php echo $article['news_content']; ?>
+                    </p>
+                    
+                </div>
+                
+                <div class="clear"></div>
+                <br />
+                <p style="padding: 0 20px 0 0;
+                    font-size: 14px; font-weight: bold;
+                    font-style: italic;
+                    text-align: right;">Ngu?n Internet</p>
+                
+            </div>
+           <!-- end: content -->
+           
+           <!-- start: news-share -->
+           <!-- end: news-share -->
+           <!-- start: news-share -->
+           <div class="news-relatedpost">
+            <h1>Các tin khác:</h1>
+            <?php 
+                foreach ($news as $new) {
+                    if($new['news_id'] != $article['news_id']){
+                        echo '<ul><li><a href="'.$frontend.'new_detail/'.$new['news_id'].'">'.$new['new_daily_name'].'</a></li></ul>';
+                    }
+                
+                } 
+             ?>
+           </div>
+            <!-- end: news-share -->
+        </div>
+        
+       
+    </div>
+    
+</div>
