24b4259bf102696e2c3cc29f00db54918a273ce7
Mr.hiepuit@gmail.com <Mr.hiepuit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-09-16 09:50:28 +0000

    tạo file index.php trong views/frontend/. File index.php giống file home.php nhưng có tác dụng gọi lại các file về sau

---
+++ trunk/Web_UI_Game/application/views/frontend/index.php
@@ -0,0 +1,83 @@
+
+<?php $path = base_url().'public/frontend/';?>
+<!DOCTYPE html> 
+<html >
+	<head><meta http-equiv="Content-Type" content="text/htm; charset=UTF-8">
+
+<title><?php echo $content; ?></title>
+<!-- start : page css here -->
+
+<link rel="stylesheet" href="<?php echo $path; ?>css/stylesheet.css" type="text/css" media="screen"/>
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo $path ;?>css/menu.css">
+
+<!-- end : page css here -->
+
+
+</head>
+<body >
+	
+<div id="wrapper">
+    <div id="main">
+        <!-- start: header -->
+        <div id="header">
+            <div class="logo">
+                <a title="Tin t?c" href="#">
+                    <img alt="Tin t?c" src="<?php echo $path ;?>images/logo.png" />
+                </a>
+            </div>
+            <div id="search">
+            </div>
+            <div id="cat_menu">
+            </div>
+        </div>
+        <!-- end: header -->
+        <!-- start: menu -->
+        <div id="menu">
+		  <?php $this->load->view('frontend/menu');?>
+	    </div>
+        <!-- end: menu -->
+        <!-- start: slideshow -->
+        <div id="slideshow">
+        </div>
+        <!-- end: slideshow -->
+        <div class="clear"></div>
+        
+        <!-- start: content -->
+        <div id="content">
+            <div class="clear"></div>
+            <div id="feature-post">
+            
+            </div>
+            <div class="clear"></div>
+                <div id="article">
+                    <div id="article-content">
+                    <div class="breadcrumb">		
+    				 You are here:&nbsp;&nbsp;  Home&nbsp;>>&nbsp;<?php echo $content ;?>
+    			    </div>    
+                    	     
+            		  <?php $this->load->view('frontend/'.$content) ;?>     	       
+                    
+                    
+                    </div>
+                 <!-- Start: sidebar -->
+                    <div id="sidebar">
+                        <?php $this->load->view('frontend/sidebar');?>
+                    </div>
+            <!-- end: sidebar -->
+                </div>
+                
+            
+        </div>
+        <!-- end: content -->
+        
+        <div class="clear"></div>
+        
+        <!-- Start: footer -->        
+        <div id="footer">
+            <?php $this->load->view('frontend/footer');?>
+        </div>
+        <!-- end: footer -->
+    </div>
+</div>
+</body>
+</html>
