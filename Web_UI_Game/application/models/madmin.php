e46012bdd07261270a5cce309a0c2580665b5577
mr.hiepuit <Mr.hiepuit@gmail.com>
2013-09-18 03:38:44 +0000

    Tạo file madmin.php để nhập xuất dữ liệu trong trang admin.

---
+++ Web_UI_Game/application/models/madmin.php
@@ -0,0 +1,22 @@
+<?php
+class Madmin extends CI_Model{
+    function __construct(){
+        parent::__construct();
+    }
+    
+    function login($user, $pass){
+        // this func will login the user in as admin in the data
+        // mathes and save it in the session
+        $sha1pass = $pass;
+        $this->db->where('username', $user);
+        $this->db->where('password', $sha1pass);
+        $query = $this->db->get('admin');
+        $return = $query->result_array();
+        if($query->num_rows() > 0){
+            return $return[0]['admin_id'];
+        }else{
+            return False;
+        }
+    }
+} 
+ ?>
