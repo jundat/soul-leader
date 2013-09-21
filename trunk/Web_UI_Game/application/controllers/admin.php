881c6717759e87e68efc36ce504f7fee1dcf94b2
Mr.hiepuit@gmail.com <Mr.hiepuit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-09-21 14:50:04 +0000

    thêm phương thức để quản lí các bài yin tức của chúng ta. Gồm có thêm, xóa và sửa tin tức.

--- trunk/Web_UI_Game/application/controllers/admin.php
+++ trunk/Web_UI_Game/application/controllers/admin.php
@@ -31,5 +31,73 @@ class Admin extends CI_Controller{
         $this->session->sess_destroy();
         redirect('admin');
     }
+    
+    function manager($action = null){
+        if($this->session->userdata('logged_in')){
+            $param = $this->uri->segment(4);
+            $data['content'] = $action;
+            $data['backend'] = base_url().'index.php/admin/manager/';
+            $data['backendimg'] = base_url().'public/backend/images';
+            switch($action){
+                case ' ':
+                    $data['content'] =  'index';
+                    break;
+                case 'func_remove_article' : 
+                    $this->Mhome->remove_article($param);
+                    $data['content'] = 'article';
+                    break;
+                case 'func_restore_article': 
+                    $this->Mhome->restore_article($param);
+                    $data['content'] = 'removed_article';
+                    break;
+                case 'func_save_add_article':
+                    if($this->Mhome->save_add_article()){
+                        $data['message'] = 'Article was added successfully';
+                    }else{
+                        $data['message'] = 'Article was not added. Please Check and try again';
+                    }
+                    
+                    $data['content'] = 'article';
+                    break;
+                case 'func_save_edit_article': 
+                    if($this->Mhome->save_edit_article()){
+                        $data['message'] = 'Article was saved successfully';
+                    }else{
+                        $data['message'] = 'Article was not saved.Please check and try again';
+                    }
+                    $data['content'] = 'article';
+                    break;
+                case 'func_delete_article':
+                    $this->Mhome->delete_article($param);
+                    $data['content'] = 'removed_article';
+                    break;
+            }
+            
+            switch($data['content']){
+                case 'article':
+                    $this->load->library('pagination');
+                    $config['base_url'] = base_url().'index.php/admin/manager/article/';
+                    
+                    $config['total_rows'] = $this->Mhome->get_total_rows_news();
+                    // $config['use_page_numbers'] = TRUE;//hien thi so trag dung
+                    $config['per_page'] = 5;
+                    $config['uri_segment'] = 4;
+                    $this->pagination->initialize($config);
+                    $data['news'] = $this->Mhome->get_news($config['per_page'], $param);
+                    break;
+                case 'remove_article':
+                    $data['removed_article'] = $this->Mhome->get_removed_news();
+                    break;
+                case 'edit_article':
+                    $data['article'] = $this->Mhome->get_article($param);
+                    break;
+                    
+            }
+            $this->load->view('backend/manager', $data);
+        }
+        else{
+            redirect('admin');
+        }
+    }
 } 
  ?>
