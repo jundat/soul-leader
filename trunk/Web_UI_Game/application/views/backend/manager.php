35c4bfde091ecd929c212b839f1d0b1461f352ac
Mr.hiepuit@gmail.com <Mr.hiepuit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-09-27 02:24:22 +0000

    Tạo file manager.php để hiển thị những thành phần mà admin đã đăng nhập thành công. Gồm header, content, menu, footer.

---
+++ trunk/Web_UI_Game/application/views/backend/manager.php
@@ -0,0 +1,28 @@
+<?php $path = base_url().'application/views/'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <title>Manager Page</title>
+    <script type="text/javascript" src="<?php echo base_url(); ?>application/libraries/ckeditor/ckeditor.js"></script>
+    <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>public/backend/css/admin.css"/>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <?php $this->load->view('backend/header'); ?>
+        </div>
+        <div id="menu">
+            <?php $this->load->view('backend/menu'); ?>            
+        </div>
+        <div id="content">
+            <?php $this->load->view('backend/'.$content); ?>
+        </div>
+        <div class="clear">
+        </div>
+        <div id="footer">
+            <?php $this->load->view('backend/footer'); ?>
+        </div>
+    </div>
+</body>
+</html>
