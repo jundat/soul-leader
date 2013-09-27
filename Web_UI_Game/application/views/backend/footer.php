dc82086e95b6e7c45bf452453aa989e54b2ef028
mr.hiepuit <Mr.hiepuit@gmail.com>
2013-09-27 23:58:38 +0000

    Trang footer dùng để hiện thị thông tin của developer của trang web. Cái này chỉ demo thui> Có gì chúng ta sẽ thiết kế để tiện dụng hơn.

---
+++ Web_UI_Game/application/views/backend/footer.php
@@ -0,0 +1,10 @@
+<p>
+    Website game gunny của môn Web và ứng dụng.
+</p>
+<p>
+    Copyright 2013 All Rights Reserved - 
+    <a href="#">Huynh Ngoc Hiep</a>
+</p>
+<p>Huynh Ngoc Hiep</p>
+<p>Số điện thoại: 01222 015 585</p>
+                      
