6ddeb03f574b5ef7bdaf572b873828906962c199
hoannguyen.uit@gmail.com <hoannguyen.uit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-12-05 16:15:23 +0000

    update page gioi thiet

--- trunk/Web_UI_Game/application/views/frontend/gioi_thieu/gioithieu_view.php
+++ trunk/Web_UI_Game/application/views/frontend/gioi_thieu/gioithieu_view.php
@@ -1,3 +1,4 @@
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -5,7 +6,7 @@
         Thông tin trò chơi - Giới thiệu Gunny 
     </title>
     <!--Load css cua page-->
-    <link href="public/frontend/css/gioithieu.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url(); ?>public/frontend/css/gioithieu.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <div id="wrapperOut">
@@ -37,21 +38,11 @@
 <!-------------------------------------------------------------------------------------------------------------->
                 <!-- Begin main content-->
                 <div id="mainContent">
-                    <!-- Begin login form-->
-                    <div class="BoxLogin">
-                       <?php
-                        $this->load->view('frontend/gioi_thieu/login');
-                       ?>
-                    </div>
-                    <!-- End login form -->
-                    
                     <div id="static">
                         <!-- Begin module article-->
                         <div class="StaticTopPanel">
                             <h2 class="TitleMain">Thông tin trò chơi</h2>
-                            <p id="breadcrumbs"> <a href="/index.html" title="Trang chủ">Trang chủ</a> &gt; <a title="Hướng dẫn người mới" href="/new-user-guide/index.html">Hướng dẫn người mới</a> &gt; <span>Giới thiệu</span> &gt; <span>Thông tin trò chơi</span></p>
                         </div>
-                        
                         <div class="StaticOuter">
     		                <div class="StaticInner">
                                 <div class="StaticMain">
