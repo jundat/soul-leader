9e78204aef325cdc9f3b2456b8171ee01ef7bb56
longpham.uit@gmail.com <longpham.uit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-12-08 08:24:21 +0000

    - Delete unused resource

--- trunk/game/Soul/Soul-Leader-Cocos2d-html5/src/resource.js
+++ trunk/game/Soul/Soul-Leader-Cocos2d-html5/src/resource.js
@@ -1,30 +1,15 @@
-var s_HelloWorld = "res/HelloWorld.png";
-var s_CloseNormal = "res/CloseNormal.png";
-var s_CloseSelected = "res/CloseSelected.png";
 var s_tBackground = "res/xvn/Background.png";
-var s_tMenuBackground = "res/xvn/Menu/_menu_review.png";
+var s_tMenuBackground = "res/xvn/Menu/bgMenu.png";
 
 var s_tPlayer = "res/xvn/Player.png";
-var s_tBallBlue = "res/xvn/Ball/Ball-blue.png";
-var s_tBallBlueSelected = "res/xvn/Ball/Ball-blue-Selected.png";
-var s_tBallGreen = "res/xvn/Ball/Ball-green.png";
-var s_tBallGreenSelected = "res/xvn/Ball/Ball-green-Selected.png";
-var s_tBallRed = "res/xvn/Ball/Ball-red.png";
-var s_tBallRedSelected = "res/xvn/Ball/Ball-red-Selected.png";
-var s_tBallYellow = "res/xvn/Ball/Ball-yellow.png";
-var s_tBallYellowSelected = "res/xvn/Ball/Ball-yellow-Selected.png";
-var s_pPlayerAnimation = "res/xvn/Ball/Player.plist";
-var s_tPlayerAnimation = "res/xvn/Ball/player.png";
 var s_pExplosion = "res/xvn/explosion.plist";
 var s_tExplosion = "res/xvn/explosion.png";
-var s_tPowerBar2 = "res/xvn/red_power_bar.png";
-var s_tPowerBar = "res/xvn/blue_power_bar.png"
 var s_tHealthBar = "res/xvn/green_health_bar.png";
+
 var s_wep1 = "res/xvn/NewItem/shit.png";
 var s_wep2 = "res/xvn/NewItem/nokia.png";
 var s_wep3 = "res/xvn/NewItem/keyboard.png";
 var s_wep4 = "res/xvn/NewItem/monitor.png";
-
 
 var s_tBall = "res/xvn/NewItem/unselected_item.png";
 var s_tBallSelected = "res/xvn/NewItem/selected_item.png";
@@ -37,27 +22,12 @@ var s_tLose = "res/xvn/lose.png";
 var s_tWin = "res/xvn/win.png";
 
 var g_resources = [
-
     { src: s_tBackground },
-    { src: s_tBallBlue },
-    { src: s_tBallBlueSelected },
-    { src: s_tBallGreen },
-    { src: s_tBallGreenSelected },
-    { src: s_tBallRed },
-    { src: s_tBallRedSelected },
-    { src: s_tBallYellow },
-    { src: s_tBallYellowSelected },
-    { src: s_pPlayerAnimation },
-    { src: s_tPlayerAnimation },
     { src: s_pExplosion },
     { src: s_tExplosion },
     { src: s_tHealthBar },
-    { src: s_tPowerBar },
-    { src: s_tPowerBar2 },
-    //image
-    { src: s_HelloWorld },
-    { src: s_CloseNormal },
-    { src: s_CloseSelected },
+    
+    //images
 
     { src: s_tBall },
     { src: s_tBallSelected },
@@ -67,22 +37,11 @@ var g_resources = [
     { src: s_wep3 },
     { src: s_wep4 },
     { src: s_tMenuBackground },
-    {
-        src: s_pChar
-    },
+    { src: s_pChar },
 
     { src: s_tPlay },
     { src: s_tPlaySelected },
 
     { src: s_tLose },
     { src: s_tWin },
-    //plist
-
-    //fnt
-
-    //tmx
-
-    //bgm
-
-    //effect
 ];
