8272796a4e384f6672fe5e54cf5d6203006562c5
hoannguyen.uit@gmail.com <hoannguyen.uit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-12-13 16:04:35 +0000

    Thêm nội dung trang giới thiêu. Sử dụng thẻ <P> và <img> để code.

--- trunk/Web_UI_Game/application/views/frontend/gioi_thieu/content.php
+++ trunk/Web_UI_Game/application/views/frontend/gioi_thieu/content.php
@@ -1,1 +1,27 @@
-<h1>Nội dung phần giới thiệu</h1>
+<p><strong>WebGame</strong> hay còn gọi là browser game là loại hình game chơi trên web, thông qua trình duyệt. Webgame ra đời và phát triển mạnh từ giữa thập niên 1990[1]. Với ưu điểm nhỏ gọn, dễ dàng để phát triển, ý tưởng game phong phú cùng nhiều thể loại, web game hiện vẫn thu hút 
+hàng triệu game thủ trên toàn thế giới. Nổi tiếng nhất 
+hiện nay phải kể đến Hobowar kế tiếp là Samurai of Legend,
+ The Wrestling Game, Travian, Tiencanh… những game luôn 
+ có số lượng người chơi online lên đến hàng chục nghìn 
+ người một lúc, cùng hàng triệu fan hâm mộ khắp 
+ thế giới[cần dẫn nguồn].</p>
+ <p>Một số game nổi tiến và rất nhiều bạn trẻ hay chơi:</p>
+ <img src="<?php echo base_url(); ?>public/frontend/images/gioithieu/content1.jpg" alt="content 1" height="300" width="500"/>
+ <br />
+ <img src="<?php echo base_url(); ?>public/frontend/images/gioithieu/content2.jpg" alt="content 2" height="300" width="500"/>
+ <br />
+ <br />
+ <p>Hẳn các bạn đã quá quen thuộc với trò chơi Warcraft (hay còn gọi là Đế chế) với các chiến thuật tấn công phức tạp. Đơn giản và nhẹ nhàng hơn, Warcraft flash không cần phải cài đặt mà hoàn toàn có thể chơi online từ bất kỳ máy nào. 
+</p>
+
+<br />
+<br />
+<p>Do đó nhóm quyết định phát triển một webgame có tính chất giải 
+trí và troll nhau là chính. Sau khi bàn bạc và thảo luận để nêu ý tưởng về
+<strong>WebGame NHÓM 7</strong> quyết định chọn thể loại game bắn nhau giống game gunny.</p>
+<p>Dưới đây là một số hình ảnh về game. Các bạn mới chơi có thể xem <a href="http://localhost/soul-leader/Web_UI_Game/huong_dan">Hướng dẫn</a>
+</p>
+<img src="<?php echo base_url(); ?>public/frontend/images/gioithieu/gametroll.jpg" alt="game troll" height="300" width="500"/>
+<img src="<?php echo base_url(); ?>public/frontend/images/gioithieu/gametroll2.jpg" alt="game troll" height="300" width="500"/>
+<img src="<?php echo base_url(); ?>public/frontend/images/gioithieu/gametroll3.jpg" alt="game troll" height="300" width="500"/>
+
