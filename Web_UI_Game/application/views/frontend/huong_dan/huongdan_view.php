43b4eea05f0694ee46466378a6df7bf3295227db
hoannguyen.uit <hoannguyen.uit@gmail.com>
2013-12-13 16:07:56 +0000

    làm phần hướng dẫn view, chỉnh sửa nội dung hình ảnh.

--- Web_UI_Game/application/views/frontend/huong_dan/huongdan_view.php
+++ Web_UI_Game/application/views/frontend/huong_dan/huongdan_view.php
@@ -51,7 +51,7 @@
                                     <div class="StaticMain">
                                         <div class="ContentBlock">
                                             <h2>Nội dung phần hướng dẫn</h2>
-                                   	        <p class="ImgCenter"><img src="http://img.zing.vn/gn/images/data/2012-04-24/so-do/so-do-huong-dan-newbies.jpg" alt="Sơ đồ hướng dẫn cơ bản" height="1517" width="500"></p>
+                                   	        <p class="ImgCenter"><img src="<?php echo base_url(); ?>public/frontend/images/huongdan/so_do_huong_dan.png" alt="Sơ đồ hướng dẫn cơ bản" height="1517" width="500"></p>
                           		        </div>
                                     </div>
                                 </div>
