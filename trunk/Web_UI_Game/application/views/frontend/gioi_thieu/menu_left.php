cd2ffd1364c2f511a393d00d59c5a576f5acb12f
hoannguyen.uit@gmail.com <hoannguyen.uit@gmail.com@1e8dde21-0cdf-637f-29dd-2bbf2f8fd311>
2013-12-13 16:06:54 +0000

    tạo menu_left.php để làm menu cho phan giới thiệu.

--- trunk/Web_UI_Game/application/views/frontend/gioi_thieu/menu_left.php
+++ trunk/Web_UI_Game/application/views/frontend/gioi_thieu/menu_left.php
@@ -1,55 +1,11 @@
 <div id="innerSide">
     <ul id="sidenav">
-        <li class="HasChild"><a href="javascript:void(0);" class="gioithieu notTrack" rel="menu1">Giới thiệu</a>
-            <ul id="menu1" >
-                <li><a href="/new-user-guide/intro/gioi-thieu.html"  title="Thông tin trò chơi">Thông tin trò chơi</a></li>
-				<li><a href="/new-user-guide/intro/bang-hoi.html" title="Hệ thống bang hội ">Hệ thống bang hội</a></li>
-				<li><a href="/new-user-guide/intro/ban-do.html" title="Bản đồ">Bản đồ</a></li>
-				<li><a href="/new-user-guide/intro/trang-bi.html" title="Hệ thống vũ khí trang bị" >Vũ khí trang bị</a></li>
-				<li><a href="/new-user-guide/intro/san-dau-gia.html" title="Sàn đấu giá" >Sàn đấu giá</a></li>
-				<li><a href="/new-user-guide/intro/thanh-tich.html"  title="Thành tích &amp; xếp hạng" class="" >Thành tích &amp; xếp hạng</a></li>
-				<li><a href="/new-user-guide/intro/dieu-khoan-su-dung.html"  title="Nguyên tắc cần biết" class="Last" >Nguyên tắc cần biết</a></li>
-            </ul>
+        <li class="HasChild"><a href="http://localhost/soul-leader/Web_UI_Game/gioi_thieu" class="gioithieu notTrack" rel="menu1">Giới thiệu</a>
+            
         </li>
-        <li><a class="huongdan notTrack" href="javascript:void(0);" rel="menu2" >Hướng dẫn</a>
-			<ul id="menu2">
-                <li><a href="/new-user-guide/basic-guild/tao-tai-khoan.html" title="Tạo tài khoản & Đăng nhập">Tạo tài khoản</a></li>
-                <li><a href="/new-user-guide/basic-guild/tao-nhan-vat.html" title="Tạo nhân vật">Tạo nhân vật</a></li>
-				<li><a href="/new-user-guide/basic-guild/thao-tac-co-ban.html" title="Thao tác cơ bản">Thao tác cơ bản</a></li>
-				<li><a href="/new-user-guide/basic-guild/giao-dien-game.html" title="Giao diện game">Giao diện game</a></li>
-				<li><a href="/new-user-guide/basic-guild/tao-phong-thi-dau.html" title="Tạo phòng thi đấu">Tạo phòng thi đấu</a></li>
-			</ul>
+        <li><a class="huongdan notTrack" href="http://localhost/soul-leader/Web_UI_Game/huong_dan" >Hướng dẫn</a>
+			
         </li>
-        <li><a class="dactrung notTrack" href="javascript:void(0);" rel="menu3"  >Tính năng</a>
-			<ul id="menu3">
-				<li><a href="/new-user-guide/functions/cuong-hoa.html" title="Cường hóa vật phẩm">Cường hóa vật phẩm</a></li>
-				<li><a href="/new-user-guide/functions/vuot-ai.html" title="Vượt ải viễn chinh">Vượt ải viễn chinh</a></li>
-				<li><a href="/new-user-guide/functions/ket-hon.html" title="Tính năng kết hôn">Tính năng kết hôn</a></li>
-				<li><a href="/new-user-guide/functions/suoi-nuoc-nong.html" title="Suôi nước nóng">Suối nước nóng</a></li>
-				<li><a href="/new-user-guide/functions/lien-server.html" title="Liên server">Liên server</a></li>
-			</ul>
-        </li>
-        <li>
-            <a class="nhiemvu" href="/bi-kip/danh-sach.html" title="Bí kíp luyện gà" rel="menu4" >Bí kíp luyện gà</a>
-        </li>
-
-		<li><a class="tinhnang notTrack" href="javascript:void(0);" rel="menu5" >Nhiệm vụ</a>
-			<ul id="menu5" >
-				<li><a href="/new-user-guide/task/nhiem-vu-chu-tuyen.html" title="Nhiệm vụ chủ tuyến">Nhiệm vụ chủ tuyến</a></li>
-				<li><a href="/new-user-guide/task/nhiem-vu-hang-ngay.html" title="Nhiệm vụ hàng ngày">Nhiệm vụ hàng ngày</a></li>
-				<li><a href="/new-user-guide/task/nhiem-vu-phu.html" title="Nhiệm vụ phụ">Nhiệm vụ phụ</a></li>
-				<li ><a href="/new-user-guide/task/nhiem-vu-su-kien.html" title="Nhiệm vụ sự kiện">Nhiệm vụ sự kiện</a></li>
-			</ul>
-		</li>
-        <li><a class="chongluadao notTrack" href="javascript:void(0);" rel="menu6" >Bí kíp chống lừa đảo</a>
-			<ul id="menu6" >
-				<li><a href="/new-user-guide/bi-kep-chong-lua-dao/cap-nhat-tai-khoan.html" title="Cập nhật tài khoản">Cập nhật tài khoản</a></li>
-				<li><a href="/new-user-guide/bi-kep-chong-lua-dao/dat-mat-khau-phuc-tap.html" title="Đặt mật khẩu phức tạp">Đặt mật khẩu phức tạp</a></li>
-				<li><a href="/new-user-guide/bi-kep-chong-lua-dao/gia-mao-cao-thu.html" title="Giả mạo cao thủ">Giả mạo cao thủ</a></li>
-				<li ><a href="/new-user-guide/bi-kep-chong-lua-dao/gia-mao-website.html" title="Giả mạo website">Giả mạo website</a></li>
-				<li><a href="/new-user-guide/bi-kep-chong-lua-dao/lua-dao-qua-kenh-chat.html" title="Lừa đảo qua kênh chat">Lừa đảo qua kênh chat</a></li>
-				<li ><a href="/new-user-guide/bi-kep-chong-lua-dao/lua-dao-qua-thu.html" title="Lừa đảo qua thư">Lừa đảo qua thư</a></li>
-			</ul>
-        </li>
+        
   </ul>
 </div>
